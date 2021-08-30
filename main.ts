5329d370a5004ec5001e3d8fa2ef4dae57358248
Zequez <zequez@gmail.com>
2021-08-30 04:53:38 -0300

    Add HTTP to HTTPS redirection

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
 import Controller from "./lib/controller.ts";
-import {
-  ensureAuthorized,
-  deleteKey,
-  UnauthorizedError,
-} from "./lib/authorization.ts";
-import bodyDecoder from "./lib/bodyDecoder.ts";
-import { respond, match, matchStatic } from "./lib/routing.ts";
-import { generateStyles } from "./lib/generateStyles.ts";
+import { UnauthorizedError } from "./lib/authorization.ts";
+import { respond } from "./lib/routing.ts";
+import generateRoutes from "./routes.ts";
+
 import runDevServer from "./lib/devServer.ts";
 
 const HTTPS_CERT = Deno.env.get("HTTPS_CERT");
@@ -37,76 +33,36 @@ const server =
     : Deno.listen({ port: PORT });
 
 console.log(
-  `[${new Date().toISOString()}] Server started at 0.0.0.0:${
+  `[${new Date().toISOString()}] ${
+    HTTPS ? "HTTPS" : "HTTP"
+  } Server started at 0.0.0.0:${
     HTTPS ? HTTPS_PORT : PORT
-  } on ${APP_ENV.toUpperCase()} mode`
+  } on ${APP_ENV.toUpperCase()} mode `
 );
 
 console.log("Using YML data controller");
 const controller = new Controller("./participants/");
 
-const staticFile = (file: string) => Deno.readTextFileSync(`./static/${file}`);
-const readStatic = (file: string) => () => staticFile(file);
+const routes = generateRoutes({ isDev, controller });
 
-const participantRegex = /^\/participants\/([a-z0-9-]+)$/;
-const routes = [
-  matchStatic("/", readStatic("index.html"), "text/html", !isDev),
-  matchStatic(
-    "/favicon.ico",
-    readStatic("favicon.svg"),
-    "image/svg+xml",
-    !isDev
-  ),
-  matchStatic("/robots.txt", readStatic("robots.txt"), "text/plain", !isDev),
-  matchStatic(
-    "/styles.css",
-    () => generateStyles(staticFile("index.html") + staticFile("app.tsx")),
-    "text/css",
-    !isDev
-  ),
-  matchStatic(
-    "/main.js",
-    async () => {
-      const { files } = await Deno.emit("./static/main.tsx", {
-        check: false,
-        bundle: "module",
-      });
-      const key = Object.keys(files).find((k) => k.endsWith("bundle.js")) || "";
-      return files[key] as string;
-    },
-    "text/javascript",
-    !isDev
-  ),
-
-  match("GET", "/participants", () =>
-    respond(200, Object.fromEntries(controller.all()))
-  ),
-  match("GET", participantRegex, ([participant]) => {
-    const data = controller.get(participant);
-    return data ? respond(200, data) : respond(404);
-  }),
-  match("POST", participantRegex, async ([participant], req) => {
-    ensureAuthorized(participant, req);
-
-    const data = await bodyDecoder(req);
-    if (data) {
-      controller.set(participant, data);
-      return respond(200, data);
-    } else {
-      return respond(400);
+// Redirect all port 80 traffic to HTTPS
+if (HTTPS) {
+  (async () => {
+    console.log("Listening on port 80 to redirect to HTTPS");
+    for await (const conn of Deno.listen({ port: 80 })) {
+      const httpConn = Deno.serveHttp(conn);
+      for await (const reqEvent of httpConn) {
+        const redirectAddress = reqEvent.request.url.replace("http", "https");
+        reqEvent.respondWith(
+          new Response("", {
+            status: 301,
+            headers: new Headers({ Location: redirectAddress }),
+          })
+        );
+      }
     }
-  }),
-  match("DELETE", participantRegex, ([participant], req) => {
-    ensureAuthorized(participant, req);
-    if (controller.get(participant)) {
-      controller.delete(participant);
-      deleteKey(participant);
-      return respond(200);
-    } else {
-      return respond(404);
-    }
-  }),
-];
+  })();
+}
 
 for await (const conn of server) {
   serveHttp(conn);
---
+++ routes.ts
@@ -0,0 +1,73 @@
+import { respond, match, matchStatic } from "./lib/routing.ts";
+import { generateStyles } from "./lib/generateStyles.ts";
+import Controller from "./lib/controller.ts";
+import { ensureAuthorized, deleteKey } from "./lib/authorization.ts";
+import bodyDecoder from "./lib/bodyDecoder.ts";
+
+const staticFile = (file: string) => Deno.readTextFileSync(`./static/${file}`);
+const readStatic = (file: string) => () => staticFile(file);
+
+export type RoutesContext = {
+  isDev: boolean;
+  controller: Controller;
+};
+
+const participantRegex = /^\/participants\/([a-z0-9-]+)$/;
+export default ({ isDev, controller }: RoutesContext) => [
+  matchStatic("/", readStatic("index.html"), "text/html", !isDev),
+  matchStatic(
+    "/favicon.ico",
+    readStatic("favicon.svg"),
+    "image/svg+xml",
+    !isDev
+  ),
+  matchStatic("/robots.txt", readStatic("robots.txt"), "text/plain", !isDev),
+  matchStatic(
+    "/styles.css",
+    () => generateStyles(staticFile("index.html") + staticFile("app.tsx")),
+    "text/css",
+    !isDev
+  ),
+  matchStatic(
+    "/main.js",
+    async () => {
+      const { files } = await Deno.emit("./static/main.tsx", {
+        check: false,
+        bundle: "module",
+      });
+      const key = Object.keys(files).find((k) => k.endsWith("bundle.js")) || "";
+      return files[key] as string;
+    },
+    "text/javascript",
+    !isDev
+  ),
+
+  match("GET", "/participants", () =>
+    respond(200, Object.fromEntries(controller.all()))
+  ),
+  match("GET", participantRegex, ([participant]) => {
+    const data = controller.get(participant);
+    return data ? respond(200, data) : respond(404);
+  }),
+  match("POST", participantRegex, async ([participant], req) => {
+    ensureAuthorized(participant, req);
+
+    const data = await bodyDecoder(req);
+    if (data) {
+      controller.set(participant, data);
+      return respond(200, data);
+    } else {
+      return respond(400);
+    }
+  }),
+  match("DELETE", participantRegex, ([participant], req) => {
+    ensureAuthorized(participant, req);
+    if (controller.get(participant)) {
+      controller.delete(participant);
+      deleteKey(participant);
+      return respond(200);
+    } else {
+      return respond(404);
+    }
+  }),
+];
